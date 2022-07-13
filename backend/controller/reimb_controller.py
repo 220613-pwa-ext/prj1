62d1410814362394eec62cf6f04c16ee2a93fd31
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2022-07-13 10:46:10 -0400

    adjust for dict return

--- backend/controller/reimb_controller.py
+++ backend/controller/reimb_controller.py
@@ -1,12 +1,14 @@
 from flask import Blueprint, request
 from exception.UserNotFound import UserNotFound
+from service.reimb_service import ReimbService
 
-cc = Blueprint('customer_controller', __name__)
+rc = Blueprint('reimb_controller', __name__)
+reimb_service = ReimbService()
 
-@cc.route('/users/<user_id>/reimbursements')
+@rc.route('/users/<user_id>/reimbursements')
 def get_all_reimb_by_user_id(user_id):
     try:
-        return 'test', 200
+        return {"reimbursements": reimb_service.get_all_reimbursements_by_user_id(user_id)}, 200
     except UserNotFound as e:
         return {
                "message": str(e)
