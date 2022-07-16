b438f28d9b5c239ab09161303430d001e78fc9a7
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2022-07-16 08:22:16 -0400

    add all user details to ID in JWT

--- backend/controller/auth_controller.py
+++ backend/controller/auth_controller.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, request, jsonify
-from exception.UserNotFound import UserNotFound
 from exception.Unauthorized import Unauthorized
 from service.auth_service import AuthService
 from flask_jwt_extended import create_access_token
@@ -7,19 +6,21 @@ from flask_jwt_extended import create_access_token
 ac = Blueprint('auth_controller', __name__)
 auth_service = AuthService()
 
+
 @ac.route('/login', methods=['POST'])
 def login():
     username = request.json.get("username", None)
     password = request.json.get("password", None)
     try:
-        role = auth_service.login(username, password)
-        access_token = create_access_token(identity={"username": username, 'role': role})
+        user = auth_service.login(username, password)
+        access_token = create_access_token(identity={"user_id": user.get_user_id(),
+                                                     "username": user.get_username(),
+                                                     "first_name": user.get_first_name(),
+                                                     "last_name": user.get_last_name(),
+                                                     "email": user.get_email(),
+                                                     "user_role": user.get_user_role()})
         return jsonify(access_token=access_token)
-    except UserNotFound as e:
-        return {
-               "message": str(e)
-           }, 404
     except Unauthorized as e:
         return {
-               "message": str(e)
-           }, 401
+                   "message": str(e)
+               }, 401
