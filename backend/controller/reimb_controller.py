6d0daac976f1339aac653536f0ab1fc6bb4b2f9e
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2022-07-18 22:28:33 -0400

    add anchors for add request and aprove with hidden attribute

--- backend/controller/reimb_controller.py
+++ backend/controller/reimb_controller.py
@@ -6,15 +6,30 @@ from flask_jwt_extended import get_jwt_identity, jwt_required
 rc = Blueprint('reimb_controller', __name__)
 reimb_service = ReimbService()
 
+
 @rc.route('/reimbursements')
 @jwt_required()
 def get_reimbursements():
     req_id = get_jwt_identity()
     try:
-        return {"reimbursements": reimb_service.get_reimbursements(req_id),
-                "user" : req_id.get('first_name'),
-                "role" : req_id.get('user_role')}, 200
+        return {"reimbursements": reimb_service.get_reimbursements_by_user_id(req_id),
+                "user": req_id.get('first_name'),
+                "role": req_id.get('user_role')}, 200
     except Unauthorized as e:
         return {
-               "message": str(e)
-           }, 401
+                   "message": str(e)
+               }, 401
+
+
+@rc.route('/reimbursements/<user_id>')
+@jwt_required()
+def get_all_reimbursements_by_user_id():
+    req_id = get_jwt_identity()
+    try:
+        return {"reimbursements": reimb_service.get_all_reimbursements(req_id),
+                "user": req_id.get('first_name'),
+                "role": req_id.get('user_role')}, 200
+    except Unauthorized as e:
+        return {
+                   "message": str(e)
+               }, 401
