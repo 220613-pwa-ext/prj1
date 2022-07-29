9d89b6369de8aafc9091209d22fe880aeadad82b
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2022-07-29 07:06:22 -0400

    add negative tests and change behavior of cancel button to clear the form fields

---
+++ backend/negative_login_tests.py
@@ -0,0 +1,96 @@
+import time
+import autoit
+from selenium import webdriver
+from selenium.webdriver import ActionChains, Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import WebDriverWait
+import selenium.webdriver.support.expected_conditions as EC
+
+from pom.login import Login
+from pom.reimb_view import ReimbView
+
+c_options = webdriver.ChromeOptions()
+c_options.add_argument("start-maximized")
+
+driver = webdriver.Chrome('./chromedriver.exe', options=c_options)
+driver.implicitly_wait(4)
+driver.get("http://127.0.0.1:5500/")
+
+login = Login(driver)
+reimb = ReimbView(driver)
+action_chain = ActionChains(driver)
+
+time.sleep(1)
+driver.find_element(By.LINK_TEXT, 'Login').click()
+
+# unsuccessful login attempt
+login.get_username_input().send_keys("JohnD80")
+time.sleep(2)
+login.get_password_input().send_keys("passwor")
+time.sleep(2)
+
+# attempt navigate to view reimbursement page
+login.get_login_button().click()
+
+# unsuccessful login attempt
+login.get_username_input().send_keys("JohnD8")
+time.sleep(2)
+login.get_password_input().send_keys("password")
+time.sleep(2)
+
+# attempt navigate to view reimbursement page
+login.get_login_button().click()
+
+
+# unsuccessful login attempt
+login.get_username_input().send_keys("JohnD8")
+time.sleep(2)
+login.get_password_input().send_keys("passwor")
+time.sleep(2)
+
+# attempt navigate to view reimbursement page
+login.get_login_button().click()
+# wait for page to load
+
+
+login.get_username_input().send_keys("JohnD80")
+time.sleep(2)
+login.get_password_input().send_keys("password")
+time.sleep(2)
+login.get_login_button().click()
+# demonstrate filter
+
+# add reimbursement
+reimb.get_add_reimbursement().click()
+time.sleep(1)
+#reimb.get_amount_input().send_keys('50')
+time.sleep(2)
+reimb.get_description_input().send_keys('Lunch - Bond with new team member')
+time.sleep(2)
+file = reimb.get_receipt_input()
+
+
+# use autoit - install -  pip install pyautoit
+action_chain.move_to_element(file).click().perform()
+time.sleep(2)
+handle = "[CLASS:#32770; TITLE:Open]"
+autoit.win_wait(handle, 60)
+autoit.control_set_text(handle, "Edit1", "C:\\Users\\Valentin\\Downloads\\receipt2.jpeg")
+autoit.control_click(handle, "Button1")
+time.sleep(4)
+select_add = Select(reimb.get_category_input())
+select_add.select_by_visible_text('Food')
+time.sleep(4)
+
+reimb.get_submit_btn().click()
+time.sleep(7)
+driver.find_element(By.ID, 'cancel-btn').click()
+
+
+
+
+
+
+time.sleep(4)
+driver.quit()
